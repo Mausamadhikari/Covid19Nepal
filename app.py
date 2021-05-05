41348f42674d8a735dbe7533f2e1b47af0ae0496
mausam <mausamadhikari20@gmail.com>
2021-05-05 15:37:20 +0545

    [updated] added app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url_for_data = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+
+data = pd.read_csv(url_for_data)
+
+data = data[data.location == "Nepal"]
+data = pd.DataFrame(data)
+data = data[
+    [
+        "date",
+        "total_cases",
+        "new_cases",
+        "total_deaths",
+        "new_deaths",
+        "icu_patients",
+        "hosp_patients",
+        "total_tests",
+        "new_tests",
+        "positive_rate",
+        "tests_per_case",
+        "tests_units",
+        "total_vaccinations",
+        "people_vaccinated",
+        "new_vaccinations",
+    ]
+]
+
+data.to_csv("covidNepal.csv",index=True)
